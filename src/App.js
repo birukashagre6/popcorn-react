feafcf5a0451bb6498875ab8e9aba9e07385b78a
birukashagre6 <mandave693@gmail.com>
2025-01-14 21:48:04 +0300

    adding the watched movies in local storage

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+    localStorage.setItem("watched", JSON.stringify(watched));
   }
 
   function handleDeleteWatched(id) {
